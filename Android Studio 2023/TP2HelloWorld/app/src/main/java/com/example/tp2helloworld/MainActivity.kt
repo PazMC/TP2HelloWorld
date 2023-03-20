caa4658d131e3748e61f81bfca4ab86f7023f0f2
Paz <di.pazcoll@gmail.com>
2023-03-20 02:52:30 -0300

    MainActivity.kt code

--- Android Studio 2023/TP2HelloWorld/app/src/main/java/com/example/tp2helloworld/MainActivity.kt
+++ Android Studio 2023/TP2HelloWorld/app/src/main/java/com/example/tp2helloworld/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.tp2helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editTextUser: EditText
+    lateinit var button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextUser = findViewById(R.id.etUser)
+        button = findViewById(R.id.toastButton)
+
+        button.setOnClickListener {
+            val text = editTextUser.text
+            Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
